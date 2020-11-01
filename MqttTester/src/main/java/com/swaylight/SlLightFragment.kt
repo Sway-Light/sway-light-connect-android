3b1903cb43b1fe73c67b7795fc282a7610101698
David <david87124@gmail.com>
2020-11-02 02:50:02 +0800

    test commit

--- MqttTester/src/main/java/com/swaylight/SlLightFragment.kt
+++ MqttTester/src/main/java/com/swaylight/SlLightFragment.kt
@@ -1,14 +1,19 @@
 package com.swaylight
 
+import android.R
+import android.graphics.drawable.Drawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.ImageView
+import androidx.core.content.ContextCompat.getDrawable
+import androidx.fragment.app.Fragment
 
 
 class SlLightFragment : Fragment() {
+
+    var ivGradientCircle:ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +24,9 @@ class SlLightFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val v = inflater.inflate(R.layout.fragment_sl_light, container, false)
+        ivGradientCircle = v.findViewById(R.id.iv_gradient_circle)
+//        val drawable: Drawable? = getDrawable(R.drawable.gradient_circle)
+
         return v
     }
 }
