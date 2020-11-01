045982e1e5f6488211208b10890828d5fc32739e
David <david87124@gmail.com>
2020-11-01 21:34:20 +0800

    1. 合併zoom/offset至同topic 2. 新增SLDisplay類別

--- library/src/main/java/com/swaylight/library/data/SLTopic.java
+++ library/src/main/java/com/swaylight/library/data/SLTopic.java
@@ -10,10 +10,12 @@ public enum SLTopic {
     LIGHT_MODE_COLOR("/mode/light/color"),
     LIGHT_MODE_OFFSET("/mode/light/offset"),
     LIGHT_MODE_ZOOM("/mode/light/zoom"),
+    LIGHT_MODE_DISPLAY("/mode/light/display"),
 
     MUSIC_MODE_COLOR("/mode/music/color"),
     MUSIC_MODE_OFFSET("/mode/music/offset"),
     MUSIC_MODE_STYLE("/mode/music/style"),
+    MUSIC_MODE_DISPLAY("/mode/music/display"),
     ;
 
     static public final String ROOT = "feeds/";
