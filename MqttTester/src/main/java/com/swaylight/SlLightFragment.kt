e91afe562b82f415da8ff16fb69489d828986a4a
David <david87124@gmail.com>
2020-11-02 19:44:07 +0800

    完成一部分custom view GradientCircle

--- MqttTester/src/main/java/com/swaylight/SlLightFragment.kt
+++ MqttTester/src/main/java/com/swaylight/SlLightFragment.kt
@@ -1,19 +1,20 @@
 package com.swaylight
 
-import android.R
-import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.core.content.ContextCompat.getDrawable
 import androidx.fragment.app.Fragment
+import com.swaylight.custom_ui.GradientCircle
 
 
 class SlLightFragment : Fragment() {
 
-    var ivGradientCircle:ImageView? = null
+    private val TAG = SwayLightMainActivity::class.java.simpleName
+
+    var gradCircle:GradientCircle? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,9 +25,12 @@ class SlLightFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val v = inflater.inflate(R.layout.fragment_sl_light, container, false)
-        ivGradientCircle = v.findViewById(R.id.iv_gradient_circle)
-//        val drawable: Drawable? = getDrawable(R.drawable.gradient_circle)
-
+        gradCircle = v.findViewById(R.id.grad_circle)
+        gradCircle!!.setOnClickListener{
+            gradCircle!!.isCheck = (!gradCircle!!.isCheck)
+            gradCircle!!.rotation = 180
+            Log.d(TAG, "isCheck " + gradCircle!!.isCheck)
+        }
         return v
     }
 }
---
+++ MqttTester/src/main/java/com/swaylight/custom_ui/GradientCircle.kt
@@ -0,0 +1,69 @@
+package com.swaylight.custom_ui
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.SweepGradient
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.View
+import com.swaylight.R
+import com.swaylight.R.styleable.*
+
+
+class GradientCircle(context: Context, attrs: AttributeSet?): View(context, attrs) {
+
+    var startColor: Int = Color.BLACK
+    var endColor: Int = Color.BLACK
+    var centerColor: Int? = null
+    var ringColor: Int = Color.BLACK
+    var rotation: Int = 0
+    var isCheck: Boolean = false
+
+    init {
+        context.theme.obtainStyledAttributes(
+                attrs,
+                R.styleable.GradientCircle,
+                0, 0).apply {
+
+            try {
+                startColor = getInteger(GradientCircle_android_startColor, 0)
+                endColor = getInteger(GradientCircle_android_endColor, 0)
+                centerColor = getInteger(GradientCircle_android_centerColor, 0)
+                ringColor = getInteger(GradientCircle_ringColor, 0)
+                rotation = getInteger(GradientCircle_android_rotation, 90)
+                isCheck = getBoolean(GradientCircle_isCheck, false)
+            } finally {
+                recycle()
+            }
+        }
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        val circlePaint = Paint()
+        val ringPaint = Paint()
+        val value = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 20F, context.resources.displayMetrics)
+
+        canvas?.rotate(rotation.toFloat(), value, value)
+        circlePaint.isAntiAlias = true
+        circlePaint.isFilterBitmap = true
+        if(centerColor == 0) {
+            circlePaint.shader = SweepGradient(value,  value, intArrayOf(startColor, endColor), floatArrayOf(0f, 1.0f))
+        }else {
+            circlePaint.shader = SweepGradient(value, value, intArrayOf(startColor, centerColor!!, endColor), floatArrayOf(0f, 0.5f, 1.0f))
+        }
+
+        ringPaint.isAntiAlias = true
+        ringPaint.isFilterBitmap = true
+        ringPaint.style = Paint.Style.STROKE
+        ringPaint.color = ringColor
+        ringPaint.strokeWidth = value * 0.1F
+        if(isCheck) {
+            canvas?.drawCircle(value, value, value*0.9F, ringPaint)
+        }
+        canvas?.drawCircle(value, value, value*0.7F, circlePaint)
+        invalidate()
+    }
+}
