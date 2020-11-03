0b616edaf2e964ad011830b3e0cc76f02dc311ee
David <david87124@gmail.com>
2020-11-03 22:01:37 +0800

    動態新增GradientCirclec

--- MqttTester/src/main/java/com/swaylight/SlLightFragment.kt
+++ MqttTester/src/main/java/com/swaylight/SlLightFragment.kt
@@ -1,20 +1,26 @@
 package com.swaylight
 
+import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import com.swaylight.custom_ui.GradientCircle
+import com.swaylight.data.GradientColor
 
 
 class SlLightFragment : Fragment() {
 
     private val TAG = SwayLightMainActivity::class.java.simpleName
 
-    var gradCircle:GradientCircle? = null
+    var gradCircles: ArrayList<GradientCircle> = arrayListOf()
+    var gradCircleGroup: LinearLayout? = null
+    var gradColorList: ArrayList<GradientColor> = arrayListOf(
+            GradientColor(Color.BLACK, Color.BLUE),
+            GradientColor(Color.WHITE, Color.RED),
+            GradientColor(Color.GREEN, Color.DKGRAY))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,12 +31,24 @@ class SlLightFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val v = inflater.inflate(R.layout.fragment_sl_light, container, false)
-        gradCircle = v.findViewById(R.id.grad_circle)
-        gradCircle!!.setOnClickListener{
-            gradCircle!!.isCheck = (!gradCircle!!.isCheck)
-            gradCircle!!.rotation = 180
-            Log.d(TAG, "isCheck " + gradCircle!!.isCheck)
-        }
+        gradCircleGroup = v.findViewById(R.id.grad_circle_group)
+        generateGradCircles()
         return v
+    }
+
+    private fun generateGradCircles() {
+        for(gradColor in gradColorList) {
+            val g = GradientCircle(context!!, null, gradColor.startColor!!, gradColor.endColor!!)
+            g.isCheck = false
+            g.ringColor = 0xFFFFFFFF.toInt()
+            g.setOnClickListener{
+                for (gc in gradCircles) {
+                    gc.isCheck = false
+                }
+                g.isCheck = true
+            }
+            gradCircles.add(g)
+            gradCircleGroup!!.addView(g)
+        }
     }
 }
